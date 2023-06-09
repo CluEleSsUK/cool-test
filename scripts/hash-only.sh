9f1f3b57e6702f29dffe2ba13ee3b83bbebb76c6
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-06-09 14:02:08 +0200

    fixed the output format and deletion in hash only

--- scripts/hash-only.sh
+++ scripts/hash-only.sh
@@ -8,18 +8,15 @@ fi
 lists_dir=$1
 hashes_dir=$2
 
-# first we clear any old hashes we've created
-rm -rf $hashes_dir/*
-
-# then we get all the files in the dir (which should only be lists)
-files=$(ls $lists_dir)
+# then we get all the files in the dir that don't start with the 'hashes-' prefix
+files=$(ls $lists_dir | grep -v "hashes-")
 
 for filename in $files; do
   contents=$(cat $lists_dir/$filename)
 
   # for every line in each file
   for current_line in $contents; do
-    # we hash it, and output the hash to a new file in the output dir
-    echo $current_line | sha256sum > $hashes_dir/$filename
+    # we hash it, strip the trailing - char and output the hash to a new file in the output dir
+    echo $current_line | sha256sum  | sed 's/\(.*\)-/\1/' > $hashes_dir/$filename
   done
 done
